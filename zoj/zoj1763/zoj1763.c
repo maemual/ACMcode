2be4040a128f2420025dfc3ddc41fef668f9c7cb
maemual <maemual@gmail.com>
2012-11-21 16:22:08 +0800

    zoj1763,the same to poj2871.

---
+++ zoj/zoj1763/zoj1763.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    double last = 0, now;
+    scanf("%lf", &last);
+    while (1){
+		scanf("%lf", &now);
+		if (now == 999)
+			break;
+        printf("%.2lf\n", now - last);
+        last = now;
+    }
+    printf("End of Output\n");
+    return 0;
+}
+
