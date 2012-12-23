698cda81d4e6a08b30abf2067a042ccfc514ba60
maemual <maemual@gmail.com>
2012-12-23 10:50:48 +0800

    CF 157,AC A && B && C.

--- CF/157/A/a.c
+++ CF/157/A/a.c
@@ -14,36 +14,17 @@ int main()
 		getchar();
 	}
 
-/*		printf("\n");
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			printf("i:%d j:%d %c",i,j,map[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	*/
 	int flag = 0;
 	for (i = 0; i < 8; i++)
 	{
-		if (map[i][0] == map[i][7])
-		{
-//			printf("%d\n",i);
-			flag = 1;
-			break;
-		}
 		for (j = 0; j < 7; j++)
 		{
 			if (map[i][j] == map[i][j+1])
 			{
-//				printf("%c",map[i][j]);
 				flag = 1;
 				break;
 			}
 		}
-//		printf("\n");
 		if (flag)
 		  break;
 	}
