8f97f7d2dca7927e56fb2c37dafa85336aa053ec
maemual <maemual@gmail.com>
2013-02-03 14:27:18 +0800

    poj2643,map.

---
+++ poj/poj2643/poj2643.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <map>
+#include <string>
+using namespace std;
+
+struct node{
+	string party;
+	int vote;
+}temp;
+int main()
+{
+	int n,m;
+	cin >> n;
+	getchar();
+	string name,party;
+	map<string,node> amap;
+	for (int i = 0; i < n; i++)
+	{
+		getline(cin,name);
+		getline(cin,party);
+		temp.party = party;
+		temp.vote = 0;
+		amap.insert(pair<string,node>(name,temp));
+	}
+	cin >> m;
+	getchar();
+	string votefor;
+	for (int i = 0; i < m; i++)
+	{
+		getline(cin,votefor);
+		map<string,node>::iterator it = amap.find(votefor);
+		if (it != amap.end())
+		{
+			(it->second).vote++;
+		}
+	}
+	int max = -1;
+	int cnt = 0;
+	string ans;
+	map<string,node>::iterator it;
+	for (it = amap.begin(); it != amap.end(); it++)
+	{
+		if ((it->second).vote == max){
+			cnt++;
+		}else if ((it->second).vote > max){
+			max = (it->second).vote;
+			ans = (it->second).party;
+			cnt = 0;
+		}
+	}
+	if (cnt == 0)
+	  cout << ans << endl;
+	else
+	  cout << "tie" << endl;
+	return 0;
+}
