b619ec284c6025b63eedc358c621994e19fa3f1a
maemual <maemual@gmail.com>
2013-03-01 10:05:23 +0800

    添加注释.

--- CF/div2/170/C/c.cpp
+++ CF/div2/170/C/c.cpp
@@ -9,10 +9,10 @@
 using namespace std;
 
 struct ss{
-	bool in;
-	bool lan[105];
+	bool in;//记录是否已经被缩点
+	bool lan[105];//记录他会的语言是哪些
 }node[105];
-bool check(ss s1,ss s2)
+bool check(ss s1,ss s2)//检查两个点之间是否有共同语言，如果有，则可以缩点
 {
 	for (int i = 1; i <= 100; i++)
 	{
@@ -37,7 +37,7 @@ int main()
 	{
 		cin >> k;
 		if (k != 0)
-			flag = true;
+			flag = true;//记录是否是这样的情况，所有人都什么语言都不会
 		for (int j = 0; j < k; j++)
 		{
 			cin >> x;
@@ -47,7 +47,7 @@ int main()
 	int p = 0;
 	int sum = n;
 	int tmp;
-	while (1)
+	while (1)//无限循环，直到没有点被缩即可退出
 	{
 		tmp = sum;
 		for (int i = 1; i <= n; i++)
@@ -63,6 +63,7 @@ int main()
 						node[j].in = false;
 						for (int k = 1; k <= m; k++)
 						{
+							//把被缩点所有会的语言给i点
 							node[i].lan[k] |= node[j].lan[k];
 						}
 						sum--;
@@ -70,11 +71,11 @@ int main()
 				}
 			}
 		}
-		if (tmp == sum)
+		if (tmp == sum)//没有点减少
 			break;
 	}
 	if (sum == n && flag == false)
-		cout << sum << endl;
+		cout << sum << endl;//如果没有点被缩，并且每个人什么语言都不会
 	else
 		cout << sum - 1 << endl;
 	return 0;
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("test.\b");
-	return 0;
-}
