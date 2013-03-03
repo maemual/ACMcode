5688d045125645ee3cb870d2da3174366e235350
maemual <maemual@gmail.com>
2013-03-03 10:50:37 +0800

    poj1312,zoj1272.Java.

---
+++ poj/poj1312/Main.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.math.*;
+public class Main {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		BigInteger base = BigInteger.valueOf(26);
+		while (true){
+			String str = in.next();
+			if (str.charAt(0) == '*')
+				break;
+			String res1,res2;
+			if (str.charAt(0) >= '0' && str.charAt(0) <= '9'){
+				res2 = str;
+				BigInteger ans = new BigInteger(str);
+				BigInteger sum = new BigInteger(str);
+				BigInteger tmp = BigInteger.ONE;
+				BigInteger res[] = new BigInteger[220];
+				int ns = 0;
+				while (true){
+					if (sum.equals(BigInteger.ZERO))
+						break;
+					res[ns++] = sum.mod(base);
+					sum = sum.divide(base);
+				}
+				for (int i = ns-1; i >= 0; i--){
+					System.out.print((char)(Integer.parseInt(res[i].toString(),10) + 'a' - 1));
+				}
+				for (int i = 0; i < 22 - ns; i++)
+					System.out.print(' ');
+				System.out.println(String.format("%,d", ans));
+			}else{
+				res1 = str;
+				BigInteger sum = BigInteger.ZERO;
+				BigInteger tmp = BigInteger.ONE;
+				for (int i = 0; i < str.length(); i++){
+					tmp = BigInteger.valueOf(str.charAt(i) - 'a' + 1);
+					sum = sum.multiply(base);
+					sum = sum.add(tmp);
+				}
+				System.out.print(res1);
+				for (int i = 0; i < 22 - res1.length(); i++)
+					System.out.print(' ');
+				res2 = String.format("%,d", sum);
+				System.out.println(res2);
+			}
+		}
+	}
+}
+
