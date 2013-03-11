09750d17975e8e1ad53553c976eba09c6254a2fb
maemual <maemual@gmail.com>
2013-03-11 21:00:11 +0800

    poj1000,java.

---
+++ poj/poj1000/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] argv){
+		Scanner input = new Scanner(System.in);
+		while (input.hasNext()){
+			int a = input.nextInt();
+			int b = input.nextInt();
+			System.out.println(a+b);
+		}
+	}
+}
