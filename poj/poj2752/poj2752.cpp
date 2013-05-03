cab3e49da2b3b807ec349689834e26e868d6c18d
maemual <maemual@gmail.com>
2013-05-03 11:10:43 +0800

    poj2752,KMP中next数组的使用。

--- poj/poj2752/poj2752.cpp
+++ poj/poj2752/poj2752.cpp
@@ -2,30 +2,39 @@
 #include <cstring>
 const int MAX_LEN = 400000;
 char s[MAX_LEN+5];
+int next[MAX_LEN+5];
+int ans[MAX_LEN+5];
 int length;
-bool check(int k)
+void getNext(char s[])
 {
-	int begin = length - k;
-	for (int i=0;i<k;i++)
+	int j = -1;
+	next[0] = -1;
+	for (int i=1; i<length; i++)
 	{
-		if (s[i] != s[begin + i])
-		  return false;
+		while (j>=0 && s[j+1] != s[i])
+		  j = next[j];
+		if (s[j+1] == s[i])
+		  j++;
+		next[i] = j;
 	}
-	return true;
+}
+void print(int x){
+	if (next[x] == -1){
+		printf("%d", x+1);
+		return ;
+	}
+	print(next[x]);
+	printf(" %d",x+1);
 }
 int main()
 {
 	while (scanf("%s",s) != EOF)
 	{
 		length = strlen(s);
-		for (int i=1;i<length;i++)
-		{
-			if (s[i-1] == s[length-1] && s[0] == s[length-i] && check(i))
-			{
-				printf("%d ",i);
-			}
-		}
-		printf("%d\n",length);
+		getNext(s);
+		int j = 0;
+		print(length-1);
+		printf("\n");
 	}
 	return 0;
 }
