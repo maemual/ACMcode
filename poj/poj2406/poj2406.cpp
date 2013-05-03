b8148063106c738630c70e80b2b60e3f8ae4e027
maemual <maemual@gmail.com>
2013-05-03 12:07:14 +0800

    poj2406,最小循环节。

--- poj/poj2406/poj2406.cpp
+++ poj/poj2406/poj2406.cpp
@@ -6,15 +6,15 @@ int next[MAX_LEN+5];
 void getNext(char s[])
 {
 	int j=-1,len;
-	len=strlen(s);
-	next[0]=-1;
-	for(int i=1;i<len;i++)
+	len = strlen(s);
+	next[0] = -1;
+	for(int i = 1; i < len; i++)
 	{
 		while(j>=0 && s[j+1]!=s[i])
 		  j=next[j];
-		if(s[j+1]==s[i])
+		if(s[j+1] == s[i])
 		  j++;
-		next[i]=j;
+		next[i] = j;
 	}
 }
 int main()
@@ -22,15 +22,14 @@ int main()
 	while (1)
 	{
 		scanf("%s",s);
-		if (s[0] == '.' && strlen(s) == 1)
+		int len = strlen(s);
+		if (s[0] == '.' && len == 1)
 		  break;
 		getNext(s);
-		int k = strlen(s)-1,pre;
-		while (next[k] != -1)
-		{
-			k = next[k];
-		}
-		printf("%d\n",strlen(s)/(k+1));
+		if (len % (len - next[len-1] - 1) == 0)
+			printf("%d\n",len / (len - next[len-1] - 1));
+		else
+			printf("1\n");
 	}
 	return 0;
 }
