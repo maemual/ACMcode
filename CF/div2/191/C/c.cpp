3b3bb153c8dc3f3199bb2241d9480dc251796aa3
maemual <maemual@gmail.com>
2013-07-05 14:34:52 +0800

    modify the C.

--- CF/div2/191/C/c.cpp
+++ CF/div2/191/C/c.cpp
@@ -4,37 +4,51 @@
 using namespace std;
 typedef long long ll;
 #define MOD 1000000007
-ll powermod(ll a,ll b,ll c)
-{
-	ll ans = 1;
-	while(b)
-	{
-		if(b&1)		//b是奇数，即二进制最后一位为1
-		   ans*=(ans*a)%c;
-		a=(a*a)%c;      //权值不断开方，同时可以模c无影响
-		b=b>>1;		//不断右移除2
+//计算a^bmodn     
+ll modexp(ll a,ll b,ll n)     
+{    
+	ll ret = 1;
+	ll tmp = a;
+	while(b){
+		if (b & 0x1) ret = ret * tmp % n;
+		tmp = tmp * tmp % n;
+		b >>= 1;
 	}
-	return ans;
-}
+	return ret;
+} 
 int main()
 {
+	/*
+	ll x;
+	cin >> x;
+	cout << modexp(2, x, MOD) << endl;
+	*/
 	string str;
 	int n;
 	cin >> str;
 	cin >> n;
 	int len = str.length();
-	long long ans1 = 0;
+	ll ans1 = 0;
+	ll ans = 0;
 	for (int i = 0; i < len; i++){
 		if (str[i] == '0' || str[i] == '5'){
-			ans1 += powermod(2, i, MOD);
+			//ans1 += modexp(2, i, MOD);
+			ll tmp1 = modexp(2, i, MOD);
+			ll tmp2 = modexp(2, len, MOD);
+			ll tmp3 = modexp(tmp2, n, MOD);
+			ans1 += tmp1 * ((tmp3 - 1) % MOD) / ((tmp2 - 1) % MOD);
+			ans += ans1 % MOD;
 		}
 	}
+	cout << ans << endl;
+	/*
 	ans1 %= MOD;
-	long long ans2 = powermod(2, (n - 1) * len - 1, MOD);
+	long long ans2 = modexp(2, (n - 1) * len, MOD);
 	ans2 %= MOD;
 	long long ans = (ans1 * ans2) % MOD;
-	cout << ans1 << endl;
-	cout << ans2 << endl;
-	cout << ans << endl;
+	//cout << ans1 << endl;
+	//cout << ans2 << endl;
+	//cout << ans << endl;
+	*/
 	return 0;
 }
