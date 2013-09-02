62bad6be16cb16375d743f824187a561762f6ffd
maemual <maemual@gmail.com>
2013-09-02 10:15:00 +0800

    OK, 4648 is right now.

---
+++ hdu/hdu4608/hdu4608.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+using namespace std;
+
+#define MAX_LEN 100005
+
+char str[MAX_LEN];
+int len;
+int sum(char s[]){
+	int tmp = 0;
+	for (int i = 0; i < len - 1; i++){
+		tmp += str[i] - '0';
+	}
+	return tmp;
+}
+int main()
+{
+	int T;
+	int s;
+	int last;
+	scanf("%d", &T);
+	while (T--){
+		scanf("%s", str+1);
+		str[0] = '0';
+		len = strlen(str);
+		s = sum(str);
+		last = str[len-1] - '0';
+		bool flag = false;
+		while (1){
+			last++;
+			if (last == 10){
+				last = 0;
+				flag = true;
+			}
+			if (flag){
+				int jin = 1;
+				for (int i = len - 2; i >= 0; i--){
+					str[i] += jin;
+					if (str[i] > '9'){
+						str[i] = '0';
+						jin = 1;
+					}else
+						jin = 0;
+				}
+				flag = false;
+				s = sum(str);
+			}
+			if ((s + last) % 10 == 0){
+				if (str[0] == '0'){
+					for (int i = 1; i < len - 1; i++)
+						printf("%c", str[i]);
+					printf("%d\n", last);
+				}else{
+					for (int i = 0; i < len - 1; i++)
+						printf("%c", str[i]);
+					printf("%d\n", last);
+				}
+				break;
+			}
+		}
+	}
+	return 0;
+}
--- hdu/hdu4648/hdu4648.cpp
+++ hdu/hdu4648/hdu4648.cpp
@@ -7,15 +7,14 @@ using namespace std;
 typedef long long ll;
 
 int left[MAX_LEN], right[MAX_LEN];
-int vis[MAX_LEN];
 int main()
 {
     int n, m, x;
     while (scanf("%d %d", &n, &m) != EOF){
         ll sum = 0;
         int num;
-        memset(vis, 0, sizeof(vis));
         memset(left, 0, sizeof(left));
+        right[0] = 0;
         for (int i = 1; i <= n; i++){
             scanf("%d", &x);
             sum += x;
@@ -23,20 +22,14 @@ int main()
             if (left[num] == 0){
                 left[num] = i;
             }
-            /*
-            if (!vis[num]){
-                vis[num] = 1;
-                left[num] = i;
-            }
-            */
             right[num] = i;
         }
         int mmax = 0;
         for (int i = 0; i < m; i++){
             if (left[i])
-            //if (vis[i])
                 mmax = max(mmax, right[i] - left[i]);
         }
+        mmax = max(mmax, right[0]);
         printf("%d\n", mmax);
     }
     return 0;
